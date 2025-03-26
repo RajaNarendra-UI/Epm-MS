d47f0192ac474c4a53ac8691df6d574c28c7f393
RajaNarendra-UI <kunarajanarendra@gmail.com>
2025-03-26 20:50:28 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          bootstrap: ['bootstrap']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
+  server: {
+    hmr: {
+      overlay: false
+    }
+  }
+})
